64b90880251d64cccefcd485938bb75aec5ba561
Christelle <bilka2004@ukr.net>
2022-10-06 21:40:30 +0300

    This is the project initial state.

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from waitress import serve
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "<span style = 'color: red'>Wrong page</span>"
+
+
+@app.route("/api/v1/hello-world-13")
+def HelloWorld():
+    return "<span style = 'color:green'><h1>Hello World 13</h1></span>"
+
+
+if __name__ == '__main__':
+    serve(app)
